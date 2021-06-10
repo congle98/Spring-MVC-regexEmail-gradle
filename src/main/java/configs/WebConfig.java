29832199844db8b2ddd350a8b41469203f2499a3
congle98 <79907265+congle98@users.noreply.github.com>
2021-06-10 22:59:44 +0700

    regex email spring project gradle

---
+++ src/main/java/configs/WebConfig.java
@@ -0,0 +1,22 @@
+package configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan("com.app.controller")
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    InternalResourceViewResolver resolver(){
+InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+    internalResourceViewResolver.setPrefix("/WEB-INF/views/");
+    internalResourceViewResolver.setSuffix(".jsp");
+    return internalResourceViewResolver;
+    }
+
+}
